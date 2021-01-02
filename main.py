41830fffd0147c01d422ba7455388ecb580d96ca
chira <chiranjayadenuwan@gmail.com>
2021-01-02 10:06:39 +0530

    use nic parser to find dob and gender using nic

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask import request
+from nic_parser.parser import Parser
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello world........."
+    print(f"{request.args}")
+    name=f"{request.args['name']}"
+    nic=f"{request.args['nic']}"
+
+    print(f"Name is {name}")
+    print(f"Nic is {nic}")
+
+    dob = Parser(f"{nic}").birth_date
+    gender = Parser(f"{nic}").gender
+    print(dob)
+    print(gender)
+
+    return f"Dob : {dob} , Gender : {gender.name}"
 
 
 if __name__ == '__main__':
